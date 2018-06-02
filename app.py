2a7552ecd6be9e935dc9f861ef32ecb23ce1613c
The Pheonix <joshua3christianx@gmail.com>
2018-06-02 00:10:01 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from functions import *
 import re, os, json
 import mimetypes
 
+myinfo = {'name':'cipherx'}
+
 clients = check_output("arp -a", shell=True).decode()
 client_ips = re.findall( r'[0-9]+(?:\.[0-9]+){3}', clients)
 host_ips = re.findall( r'Interface: [0-9]+(?:\.[0-9]+){3}', clients)
@@ -27,9 +29,7 @@ def index():
 
 @app.route('/info')
 def getInfo():
-	myinfo = {'name':'cipherx'}
-	myinfo = json.dumps(myinfo)
-	return myinfo
+	return json.dumps(myinfo)
 
 @app.route('/clients')
 def getClients():
@@ -42,43 +42,52 @@ def reroute():
 	basepath = ""
 	content = None
 
+	extended = prepareUrl(request.args["extended"])
+	print("ex", extended)
+
 	goto_url = prepareUrl(request.args["url"])
 	basename = os.path.basename(goto_url)
 	goto_baseurl = getDomain(goto_url)
 	clean_baseurl = cleanUrl(goto_baseurl)
-	print("url = {}, goto_baseurl = {}, clean_baseurl = {}, basename = {}".format(goto_url, goto_baseurl, clean_baseurl, basename))
-	if cleanUrl(getDomain(request.base_url)) in host_ips:
-		print("looped!")
-		return "[looping]"
+	url_root = cleanUrl(getDomain(request.base_url))
+	print("\nurl_root = {}\nurl = {}\ngoto_baseurl = {}\nclean_baseurl = {}\nbasename = {}".format(url_root, goto_url, goto_baseurl, clean_baseurl, basename))
+
+	if clean_baseurl in clients:
+		content = open_uri(goto_url)
+		if content != None:
+			print("url request resolved by imediate client = {}".format(clean_baseurl))
+		# print(content.headers)
+		# content_type = content.headers['content-type']
+		# extension = mimetypes.guess_extension(content_type)
+
+		# mime_type = mime.guess_type(goto_url)
 
 	else:
-		if clean_baseurl in clients:
-			content = open_uri(goto_url)
-			if content != None:
-				print("url request resolved by imediate client = {}".format(clean_baseurl))
-			# print(content.headers)
-			# content_type = content.headers['content-type']
-			# extension = mimetypes.guess_extension(content_type)
-
-			# mime_type = mime.guess_type(goto_url)
+		if url_root in host_ips:
+			print("looped!")
+			return "[looping]"
 
 		else:
 			print("checking if clients can route url resolve")
 			for client in clients:
-				content =  open_uri(bindUrl(client)+"/goto?url="+goto_url)
+				content =  "{}/goto?url={}&extended={}".format(open_uri(bindUrl(client), goto_url, myinfo["name"]))
 				if content != None:
 					print("url request resolved by client = {}".format(client))
 					break
 
-		if content != None:
-			data = content
+	if content != None:
+		data = content
+		if extended != myinfo["name"]:
+			return data
+
+		else:
 			if basename.strip() == "":
 				basename = 'tempfile'+extension
 			basepath = "/temp/"+basename
 			print("basepath = {}".format(basepath))
 			saveFile("static"+basepath, data)
-		print("basepath = {}".format(basepath))
-		return basepath
+			print("basepath = {}".format(basepath))
+			return basepath
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
